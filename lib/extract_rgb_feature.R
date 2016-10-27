b84ed56f2967b8f75c7df8c52c98489cc4974cff
Alex Saez <as3171@columbia.edu>
2016-10-26 20:35:25 -0400

    minor changes

--- lib/extract_rgb_feature.R
+++ lib/extract_rgb_feature.R
@@ -8,17 +8,13 @@ extract_rgb_feature <- function(img){
   
   mat <- imageData(img)
   
-  nR = 10
-  nG = 10
-  nB = 10
-
-  rBin = seq(0, 1, length.out=nR)
-  gBin = seq(0, 1, length.out=nG)
-  bBin = seq(0, 1, length.out=nB)
+  rBin = seq(0, 1, length.out=10)
+  gBin = seq(0, 1, length.out=10)
+  bBin = seq(0, 1, length.out=10)
   
-  freq_rgb = as.data.frame(table(factor(findInterval(mat[,,1], rBin), levels=1:nR), 
-                                  factor(findInterval(mat[,,2], gBin), levels=1:nG), 
-                                  factor(findInterval(mat[,,3], bBin), levels=1:nB)))
+  freq_rgb = as.data.frame(table(factor(findInterval(mat[,,1], rBin), levels=1:10), 
+                                 factor(findInterval(mat[,,2], gBin), levels=1:10), 
+                                 factor(findInterval(mat[,,3], bBin), levels=1:10)))
   
   rgb_feature = as.numeric(freq_rgb$Freq)/(ncol(mat)*nrow(mat)) # normalization
   
