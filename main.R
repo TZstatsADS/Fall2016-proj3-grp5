bb6444631cb41fe2a210f61483eff78c2905e7d6
Alex Saez <as3171@columbia.edu>
2016-11-02 13:45:15 -0400

    fixed

--- main.R
+++ main.R
@@ -3,41 +3,7 @@
 #############################################
 
 library(data.table)
-<<<<<<< HEAD
-
-# img_train_dir <- "./data/images/"
-# img_test_dir <- "./data/testing set/"
-
-### Import matrix of features X and create vector of labels y
-load("./output/features2.RData")
-y = c(rep(1,1000), rep(0, 1000))
-
-#####Add feature names
-siftname = vector()
-for(i in 1:5000){
-  siftname[i]<-paste0("SIFT",i)
-}
-for(i in 5001:6000){
-  siftname[i]<-paste0("Color",i-5000)
-}
-colnames(X)<-siftname
-
-# ### Construct visual feature
-# source("./lib/feature.R")
-# tm_feature_train <- system.time(dat_train <- feature(img_train_dir, "img_zip_train"))
-# tm_feature_test <- system.time(dat_test <- feature(img_test_dir, "img_zip_test"))
-# save(dat_train, file="./output/feature_train.RData")
-# save(dat_test, file="./output/feature_test.RData")
-
-
-
-####### BASELINE MODEL: depth=1 (tree stumps) ###########################################################
-
-# Model selection with cross-validation: hhoosing between different values of shrinkage for GBM
-source("./lib/train.R")
-=======
 source("./lib/feature.R")
->>>>>>> origin/master
 source("./lib/test.R")
 source("./lib/train.R")
 
