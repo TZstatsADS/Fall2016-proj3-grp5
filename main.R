e64f915bb528811a576f8825f24d31f3408a800e
Alex Saez <as3171@columbia.edu>
2016-11-02 16:28:54 -0400

    final

--- main.R
+++ main.R
@@ -9,13 +9,13 @@ source("./lib/train.R")
 
 
 # load provided features:
-X0 = fread('./data/sift_features.csv')
+X0 = fread('./data/sift features_test.csv')
 X0 = t(as.matrix(X0))
 
 
 # Construct new visual features:
 t = proc.time()
-image_dir = "./data/images/"
+image_dir = "./data/images_test/"
 X1 = feature(image_dir)
 cat("Feature extraction time:", (proc.time()-t)[3], " seconds \n")
 
@@ -35,6 +35,7 @@ models = train(X, y, saveas='./output/BL_and_ADV_models.RData')
 # make prediction
 load("./output/BL_and_ADV_models.RData")
 t = proc.time()
-pred = test(models$ADV_model, X)
+pred_BL = test(models$BL_model, X)
+pred_ADV = test(models$ADV_model, X)
 cat("Prediction time:", (proc.time()-t)[3], " seconds \n")
 
